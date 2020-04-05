01fe35489a4c86679ca64755ff75c956910cd0e4
Anthony DI STEFANO <anthony.distefano@hotmail.fr>
2020-04-05 19:25:16 +0200

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import Home from "@/pages/Home";
+import "@public/assets/style.css";
+
+ReactDOM.render(<Home />, document.getElementById("app"));
